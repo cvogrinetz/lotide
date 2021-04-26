6c3822c032cc9171f8891e661cba8fcd827dfae9
Cecil Vogrinetz <cvogrinetz@gmail.com>
2021-04-26 22:56:01 +0000

    Added tail function. Grabs all elements after first index. It returns a new array of the tail indexes

---
+++ tail.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👽👽👽 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🥵️🥵️🥵️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(arr) {
+  let newArr = [];
+
+  newArr = arr.slice(1, arr.length);
+  return newArr;
+};
+
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
