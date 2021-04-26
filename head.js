c62a2ef4ee3bf05178a2fab1319c12b0295000c2
Cecil Vogrinetz <cvogrinetz@gmail.com>
2021-04-26 22:34:09 +0000

    Added head function. Used to grab first index of array

---
+++ head.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👽👽👽 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🥵️🥵️🥵️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(arr) {
+  return arr[0];
+};
