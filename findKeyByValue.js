1bbdfc5d7c7f70f4dc3ec0a7453abfac9558a95b
Cecil Vogrinetz <cvogrinetz@gmail.com>
2021-04-28 21:11:47 +0000

    Added the findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👽👽👽 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🥵️🥵️🥵️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, value) {
+  const keys = Object.keys(obj);
+  
+  for(const key of keys) {
+    if(obj[key] === value) {
+      return key;
+    }     
+  }
+  return undefined;
+};
+
+
+const bestTVShowsByGenre = {      
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+//assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+//assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
