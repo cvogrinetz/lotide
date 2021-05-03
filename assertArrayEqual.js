988076186d219bf37e91a0a104d47972f12831b4
Cecil Vogrinetz <cvogrinetz@gmail.com>
2021-05-03 20:45:33 +0000

    Refractored assertArrayEqual function

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,11 +1,15 @@
+const eqArrays = require('./eqArrays');
+
+
 const assertArrayEquals = function(arr1, arr2) {
 
-  if (JSON.stringify(arr1) === JSON.stringify(arr2)) {
+
+  if (eqArrays(arr1, arr2)) {
+
     console.log(`👽👽👽 Assertion Passed: ${arr1} === ${arr2}`);
-    return true;
   } else {
     console.log(`🥵️🥵️🥵️ Assertion Failed: ${arr1} !== ${arr2}`);
-    return false;
   }
 };
 
+module.exports = assertArrayEquals;
