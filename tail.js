02172734e81446a7d70364be2db982691ad5e71b
Cecil Vogrinetz <cvogrinetz@gmail.com>
2021-05-03 20:07:39 +0000

    Refractored tail and made a test file for it

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👽👽👽 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🥵️🥵️🥵️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(arr) {
   let newArr = [];
 
@@ -13,6 +5,4 @@ const tail = function(arr) {
   return newArr;
 };
 
-//const words = ["Yo Yo", "Lighthouse", "Labs"];
-//tail(words);
-//assertEqual(words.length, 3);
+module.exports = tail;
