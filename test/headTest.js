130484e0d1f92323a3fd110f16dea2881de7a99e
Cecil Vogrinetz <cvogrinetz@gmail.com>
2021-05-03 21:28:25 +0000

    Refractored headTest to use Mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,25 @@
+const assert = require('chai').assert;
 const head = require('../head')
-const assertEqual = require('../assertEqual');
 
-assertEqual(head([1,2,3]), 1); 
+
+
+describe('#head', () => {
+
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it('returns 5 for [5]', () => {
+    assert.strictEqual(head(['5']), '5')
+  });
+
+  it('returns hello for ["hello", "Maybe", "Bye"]', () => {
+    assert.strictEqual(head(["hello", "Maybe", "Bye"]), "hello")
+  })
+});
+
+
+
 
 
 
