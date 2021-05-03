0f8ca68270b9b128412155c36d50817b84f22350
Cecil Vogrinetz <cvogrinetz@gmail.com>
2021-05-03 20:33:51 +0000

    Refractored eqArrays and added its test file

--- eqArrays.js
+++ eqArrays.js
@@ -1,32 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👽👽👽 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🥵️🥵️🥵️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
-// make a function to compare 2 arrays together
-// should output a pass or fail when compared
-// need to fix undefined return problem
-
-const eqArrays = function(arr1, arr2) {
+const eqArrays = function (arr1, arr2) {
 
   if (JSON.stringify(arr1) === JSON.stringify(arr2)) {
-    console.log("True");
-    return true;
+    return true
   } else {
-    console.log("False");
-    return false;
+    return false
   }
 };
 
-// USED TO TEST ABOVE FUNCTION
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-
-//eqArrays([1, 2, 3], [1, 2, 3]) // => true
-//eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-//eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-//eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+module.exports = eqArrays;
