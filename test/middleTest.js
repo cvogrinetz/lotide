76fce5b67d5165ff75b4b061ba814cf953ad4515
Cecil Vogrinetz <cvogrinetz@gmail.com>
2021-05-03 22:03:31 +0000

    Modified middleTest and tailTest to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,14 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArrayEquals = require('../assertArrayEqual');
 
 
+describe('#Middle', () => {
 
-assertArrayEquals(middle([1,2,3]), [2]);
-assertArrayEquals(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  it('should return [2] if input is [1, 2, 3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
 
+  it('should return [3, 4] if input is [1, 2, 3, 4, 5, 6]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,14 @@
+const assert = require('chai').assert
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
 
+describe('#tail', () => {
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+  it('should return ["Lighthouse", "Labs"] when input is ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"])
+  });
+
+  it('should return [2, 3, 4, 5, 6, 7, 8, 9] when input is [1, 2, 3, 4, 5, 6, 7, 8, 9]', () => {
+    assert.deepEqual(tail([ 1, 2, 3, 4, 5, 6, 7, 8, 9]), [ 2, 3, 4, 5, 6, 7, 8, 9])
+  });
+});
